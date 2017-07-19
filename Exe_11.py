c711620d11ecdfe83ed3b63cb30dea4457438c96
Binh D. Nguyen <stepup.ored@gmail.com>
2017-07-19 04:09:47 +0300

    Update exercise of list

--- Exe_11.py
+++ Exe_11.py
@@ -42,6 +42,58 @@ print("New source:", list[5])
 # ---------------------------------------------------------------------------
 
 # 3. Built-in List Functions and Methods
-# 3.1 Compare two lists
-L1, L2 = ['a', 'b', 'c'], ['b', 'a', 'c']
-print(cmp(L1, L2))
+L1, L2 = ['a', 'b', 'c'], ['b', 'a', 'c', "me"]
+
+#print(cmp(L1, L2))     # Compare two lists
+
+print(len(L1))
+
+print("Max(L2):", max(L2))
+
+print("Min(L2):", min(L2))
+
+# 4. List methods
+L = [1, 12, 123, 'a', 'zara', 1, 'big-hero']
+
+L.append("ily")
+print(L)
+
+print(L.count(1))
+print(L.count('zara'))
+
+seq = [2000, 'many', ["world"]]
+L.extend(seq)
+print(L)
+
+print(L.index('a'))             # 3
+#print(L.index('world'))         # error
+print(L.index(['world']))       # 10
+
+# Method: list.insert(index, obj)
+L = [1, '2', 'three']
+print(L)
+L.insert(2, '2-ins')
+print(L)
+
+# Pop elements
+print(L.pop(1))
+print(L)
+print(L.pop())
+print(L)
+
+# Remove elements
+L = [1, '2', 'three', 1, 2, 3, '3', '1']
+print(L)
+L.remove(1)
+print(L)
+
+# Reverse list
+L.reverse()
+print(L)
+L.reverse()
+print(L)
+
+# Sort list
+L = ['one', 'two', 'three', 'four', 'five']
+L.sort()
+print(L)
