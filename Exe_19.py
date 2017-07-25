04267ad9a8c1a05829e8e5fb1f42157362da9bb2
Binh D. Nguyen <stepup.ored@gmail.com>
2017-07-25 04:25:15 +0300

    Update full version of regular expressions

--- Exe_19.py
+++ Exe_19.py
@@ -71,3 +71,77 @@ print("Phone Number : ", num)
 # Remove anything other than digits
 num = re.sub(r'\D', "", phone)
 print("Phone Number : ", num)
+
+
+# Regular-Expression Examples
+"""
++++ Literal characters
+python    : match "python"
+
++++ Character classes
+[Pp]ython : match "Python" or "python"
+rub[ye]   : match "ruby" or "rube"
+[aeiou]   : match any lowercase vowel
+[0-9]     : match any digit; same as [01233456789]
+[a-z]     : match any lowercase ASCII letter
+[A-Z]     : match any uppercase ASCII letter
+[a-zA-Z0-9] : match any of the above
+[^aeiou]  : match anything other than a lowercase vowel
+[^0-9]    : match anything other than a digit
+
++++ Special Character Classes
+.    : match any character except new line
+\d   : match a digit: [0-9]
+\D   : match a non-digit: [^0-9]
+\s   : match a whitespace character: [\t\r\n\f]
+\S   : match a non-whitespace: [^\t\r\n\f]
+\w   : match a single word character: [A-Za-z0-9_]
+\W   : match a non-word character: [^A-Za-z0-9_]
+"""
+
+"""
++++ Repetition cases
+ruby?     : match "rub" or "ruby": the 'y' is optional
+ruby*     : match "rub" plus 0 or more 'y'
+ruby+     : match "rub" plus 1 or more 'y'
+\d{3}     : match exactly 3 digits
+\d{3,}    : match 3 or more digits
+\d{3,5}   : match 3, 4 or 5 digits
+
++++ Nongreedy repetition
+<.*>      : Greedy repetition: matches "<python>perl>"
+<.*?>     : Nongreedy: matches "<python>" in "<python>perl>"
+
++++ Grouping with Parentheses
+\D\d+     : No groups: + repeats \d
+(\D\d)+   : Grouped: + repeat \D\d pair
+([Pp]ython(,)?)+ : Match "Python", "Python ,python, python", etc
+
++++ Backreferences
+([Pp]ython&\1ails   : match python&pails or Python&Pails
+(['"][^\1]*\1       : Single or double-quoted string. \1 matches whatever the 1st
+                      group matched. \2 matches whatever the 2nd group matched, etc
+
++++ Alternatives
+python|perl   : match "python" or "perl"
+rub(y|le)     : match "ruby" or "ruble"
+Python(!+|\?) : "Python" followed by one or more ! or one ?
+"""
+
+"""
++++ Anchors (this needs to specify match position)
+^Python     : match "Python" at the start of a string or internal line
+Python$     : match "Python" at the end of a string or line
+\APython    : match "Python" at the start of a string
+Python\Z    : match "Python" at the end of a string
+\bPython\b  : match "Python" at a word boundary
+\brub\B     : \B is non-word boundary: match "rub" in the "rube" and "ruby" but not alone
+Python(?=!) : match "Python", if followed by an exclamation point
+Python(?!!) : match "Python, if not followed by an exclamation point
+
++++ Special Syntax with Parentheses
+R(?#comment)  : match "R". All the rest is a comment
+R(?i)uby      : Case-insentitive while matching "uby"
+R(?i:uby)     : Same as above
+rub(?:y|le))  : group only without creating \1 back reference.
+"""
